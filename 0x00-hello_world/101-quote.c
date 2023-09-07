ec5c1c5f0d232816a26741fca512616a71fb3adc
Teddy <papateddyetyang@gmail.com>
2023-09-07 15:16:54 -0700

    Printing and giving out to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * prints - main "and that piece of art is useful" -Dora Korpar, 2015-10-19
+ * followed by newline to standard error
+ * Return: 1
+ */
+int putchar(void);
+{
+	int putchar("and thar piece of art is useful"" - Dora korpar, 2015-10-19\n")
+	return (1);
+}
+

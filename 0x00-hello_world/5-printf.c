b0498ebc4538bced6f963cdd1b3e3bb3742e1fde
Teddy <papateddyetyang@gmail.com>
2023-09-07 11:35:05 -0700

    Priniting using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}

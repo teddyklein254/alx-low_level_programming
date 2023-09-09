45de0e7d6c17e0f347db80398062f7286955880c
Teddy <papateddyetyanga2gmail.com>
2023-09-09 08:57:42 -0700

    Printing positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 /*more headers goes there */
 
 /*betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
@@ -14,15 +15,11 @@ int main(void)
 	/* your code goes there */
 	{
 	if (n > 0)
-	
 		printf("%d is positive\n", n);
-	else if 
-		(n == 0)
-	
-		printf("%d is zero\n", n);
+	else if (n == 0)
+	printf("%d is zero\n", n);
 	else
-	
 		printf("%d is negative\n", n);
 	}
-	return 0;
+	return (0);
 }

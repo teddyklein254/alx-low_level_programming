7cb2ec0041b0e5bacb5560b3c271650e160bef3a
Teddy <papateddyetyanga2gmail.com>
2023-09-10 13:27:46 -0700

    Printing alphabet in reverse using the c operator

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entering data for lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char lowerCase;
+	for (lowerCase = 'z'; lowerCase >= 'a'; lowerCase--)
+		putchar(lowerCase);
+	putchar('\n');
+
+	return (0);
+}

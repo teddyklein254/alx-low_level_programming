18bc2f2e4b1b4cea687f52deb878132f2d057a1c
Teddy <papateddyetyanga2gmail.com>
2023-09-10 14:18:07 -0700

    Printing combinations of first number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - point of inputs
+ * Return: 0
+ */
+int main(void)
+{
+	int firstDigit;
+
+	for (firstDigit = '0'; firstDigit <= '9'; firstDigit++)
+		putchar(firstDigit);
+	if (firstDigit != '9')
+		putchar(',');
+	putchar(' ');
+	return (0);
+}

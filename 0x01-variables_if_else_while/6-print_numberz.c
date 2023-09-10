4db228bc1025078c3c914b92c2edc8c92b94296e
Teddy <papateddyetyanga2gmail.com>
2023-09-10 12:49:25 -0700

    Printing all single digits numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entering data input
+ *Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int e;
+	for (e = '0'; e <= '9'; e++)
+		putchar(e);
+	putchar('\n');
+	return (0);
+}

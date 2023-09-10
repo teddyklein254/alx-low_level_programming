160fd9cc004aa8154d30aacdfc126612cb12fc7e
Teddy <papateddyetyanga2gmail.com>
2023-09-10 13:42:48 -0700

    Printing hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entering data used for the abse
+ * Return: 0
+ */
+int main(void)
+{
+	int baseNumber;
+	char lowerCase;
+
+	{
+		for (lowerCase = '0'; baseNumber <= '9'; baseNumber++)
+		putchar(baseNumber);}
+	{
+	for (lowerCase = 'a'; baseNumber <= 'f'; lowerCase++)
+		putchar(lowerCase);
+	}
+	putchar('\n');
+	return (0);
+}

f8b1747a5aed4f3ac49c21f8f15a2fcaa16ff932
Teddy <papateddyetyanga2gmail.com>
2023-09-10 12:01:45 -0700

    Printing alphabet in lowercase followed by a newline

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+	for (s = '0'; s < '9'; s++)
+		printf("%i", s);
+	printf("\n");
+	return (0);
+}

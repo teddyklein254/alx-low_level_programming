6c475dec207e846c7dff81b6f52f8fed67599b3a
Teddy <papateddyetyanga2gmail.com>
2023-09-11 12:39:09 -0700

    Printing last digit this proghram works bes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 		else if (lastDigit < 6 && lastDigit != 0)
 		{
 			printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
-			printf("/n");
+			printf("\n");
 		}
 		return (0);
 }

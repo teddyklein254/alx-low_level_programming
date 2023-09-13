f4175077426002b2af7be24490ab0451e3548ca4
Teddy <papateddyetyanga2gmail.com>
2023-09-13 14:05:02 -0700

    Printing every jack bauer minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,12 @@ void jack_bauer(void)
 {
 	int min_1, min_2, min_3, min_4;
 	{
-		for(min_1 = 0; min_1 <= 2; min_1++)
+		for (min_1 = 0; min_1 <= 2; min_1++)
 		{
-		for  (min_2 = 0; min_2 <=9; min_2++)
+		for (min_2 = 0; min_2 <= 9; min_2++)
 		{
 
-		if ((min_1 <= 1 && min_2 <= 9) || (min_1 <= 2 && min_2 <= 3))
+		if((min_1 <= 1 && min_2 <= 9) || (min_1 <= 2 && min_2 <= 3))
 		{
 			for (min_3 = 0; min_3 <= 5; min_3++)
 			{

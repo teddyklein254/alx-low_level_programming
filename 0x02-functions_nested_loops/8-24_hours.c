08f13e7fbed0c3c66629c016703cc26d8c569624
Teddy <papateddyetyanga2gmail.com>
2023-09-13 13:19:34 -0700

    Printing every jack bauer minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * check - evry minute of jack Bauer
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int min_1, min_2, min_3, min_4;
+		for(min_1 = 0; min_1 <= 2; min_1++)
+		for  (min_2 = 0; min_2 <=9; min_2++)
+		if ((min_1 <= 1 && min_2 <= 9) || (min_1 <= 2 && min_2 <= 3))
+
+			for (min_3 = 0; min_3 <= 5; min_3++)
+					for (min_4 = 0; min_4 <= 9; min_4++)
+						_putchar(min_1 + '0');
+					_putchar(min_2 + '0');
+					_putchar(58);
+					_putchar(min_3 + '0');
+					_putchar(min_4 + '0');
+					_putchar('\n');
+}

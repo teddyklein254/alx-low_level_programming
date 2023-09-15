06a0a5d37e1d802115934ef715426273f9738cfe
Teddy <papateddyetyanga2gmail.com>
2023-09-15 15:00:22 -0700

    Printing numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 /**
  * main - code for printing numbers ten times
+ * prints out 10 times number
  * Return: 0
  */
 void more_numbers(void)
 {
 	int num1, num2;
+
 	for (num2 = 0; num2 < 9; num2++)
 		for (num1 = 0; num1 <= 14; num1++)
 		{
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
 /** print diagonal - for the terminal
  * @n: number of times of diagonal
  * Return: 0
  */
 void print_diagonal(int n)
 {
-	int x, y;
+	int i;
 
 	if (n <= 0)
-		putchar('\n');
-	else
-		for (x = 0; x < n; x++)
-			for (y = 0; y < x; y++)
-				putchar(32);
-	putchar(92);
+	{
 	putchar('\n');
+	}
+       	else 
+	{
+		for (i = 1; i <= n; i++)
+	{
+	putchar(' ', i - 1);
+	putchar('\\');
+	putchar('\n');
+        }
+    }
 }

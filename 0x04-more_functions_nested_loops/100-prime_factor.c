0e645fa0440e9bdeb2593f32c0c5e671401e76aa
Teddy <papateddyetyanga2gmail.com>
2023-09-15 12:51:44 -0700

    rogram that finds and prints the largest prime factor of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print large factor
+ * Return: 0
+ */
+int main(void)
+{
+	long x, maxf;
+	long num = 612852475143
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	if (number % x == 0)
+		maxf = number / x;
+	printf("ld\n", maxf);
+	return (0);
+}

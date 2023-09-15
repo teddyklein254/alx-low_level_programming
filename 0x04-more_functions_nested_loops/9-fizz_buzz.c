9e186207b6214bc43f60f03e3371814f84227f00
Teddy <papateddyetyanga2gmail.com>
2023-09-15 14:25:54 -0700

    Fizz buzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,20 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 
-	if((x % 3 == 0) && (x % 5 == 0))
+	if ((x % 3 == 0) && (x % 5 == 0))
 
 	printf("FizzBuzz");
 
-	else if(x % 3 == 0)
+	else if (x % 3 == 0)
 	printf("Fizz");
-	else if(x % 5 == 0)
+	else if (x % 5 == 0)
 	printf("Buzz");
 	else
 	{
 	printf("%d", x);
 	}
 	if (x != 100)
-	printf(" ");
+	printf("  ");
 	printf("\n");
 	return (0);
 }

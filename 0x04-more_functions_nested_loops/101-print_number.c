8b1cb67fae7e9ac37c3720187df127401d8765cf
Teddy <papateddyetyanga2gmail.com>
2023-09-15 13:46:08 -0700

    bneuoeivbre

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,17 +3,20 @@
 
 /**
  * print_number - integers
+ * @n: integer
  * Return: Nothing!
  */
+int _putchar(char c);
 void print_number(int n)
 {
-	unsigned int k = n;
-	if (n < 0)
+        unsigned int k = n;
+        if (n < 0)
 		n *= -1;
-	k = n;
-	putchar('-');
-	k /= 10;
+        k = n;
+        _putchar('-');
+        k /= 10;
+	
 	if (k != 0)
-		print_number(k);
-	putchar((unsigned int) n % 10 + '0');
-}
+	print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+}              

d9cdff4b00263d9706ef6f023b1ab5ec43e501fe
Teddy <papateddyetyanga2gmail.com>
2023-09-18 14:34:32 -0700

    Printing every character from first char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print one character out of the two
+ * @starting first one
+ * @str: input
+ * Return: input
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+		y++;
+	longi++;
+
+	t = longi - 1;
+	for (o = 0; 0 <= t; o++)
+	if (o % 2 == 0)
+		_putchar(str[o];
+				_putchar('\n');
+}

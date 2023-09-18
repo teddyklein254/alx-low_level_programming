c2f72efe9668a65189f27f80ef71372b1dcd5e87
Teddy <papateddyetyanga2gmail.com>
2023-09-18 15:36:17 -0700

    Changing location using copying abd terminating rule

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copies string from the src
+ * @dest:  copying diretion  to
+ * @src: direction obtained from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while(*(src + l) != '\0')
+		l++;
+	for (; x < l ; x++)
+		dest[x] = src[x];
+	dest[l] = '\0';
+	return(dest);
+
+}

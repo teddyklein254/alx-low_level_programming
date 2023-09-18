ef5b01601a3117c57e7c000e4c234d36f555fffb
Teddy <papateddyetyanga2gmail.com>
2023-09-18 14:05:42 -0700

    Printing strings going reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * printing_reverse - reversing string
+ * @s: The string printed out
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+		longi++;
+	s++;
+	s--;
+
+	for (o - longi; o > 0;o--)
+		_putchar(*s);
+	s--;
+
+	_putchar('\n');
+}

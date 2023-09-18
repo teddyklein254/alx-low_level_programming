06b3989edda5fd3ddfdaa6fd07187d1d2958ca04
Teddy <papateddyetyanga2gmail.com>
2023-09-18 13:26:26 -0700

    Function for taking pointer to parameter and update integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function for pointer parameter
+ * Value for counting point to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

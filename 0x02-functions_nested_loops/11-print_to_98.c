290034e316c97f4db1ab38f37d1fd932611c7685
Teddy <papateddyetyanga2gmail.com>
2023-09-18 16:38:16 -0700

    Printing strings going reverse

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+void print_to_98(int n)
+{
+	{
+		for (;n <= 98; n++)
+	}
+		if (n == 98)
+			printf("%d", n);
+		printf("\n");
+
+		break;
+}
+
+	else
+{
+	for (;n >= 98; n--)
+		if (n == 98)
+			printf("%d", n);
+	printf("\n");
+	break;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
+void print_to_98(int n);

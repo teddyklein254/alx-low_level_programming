111a4c0e75eecc884867652d142753e27254b9c2
Teddy <papateddyetyanga2gmail.com>
2023-09-21 01:51:20 -0700

    concatenation of strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates strings
+ * @s: gives concated string
+ * Return: 1
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+		*dest = *src;
+	dest++;
+	src++;
+
+	*dest = '\0';
+	return 0;
+}

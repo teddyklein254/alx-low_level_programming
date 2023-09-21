dd0e61422763f7462a77c10f75d9d0465d488c86
Teddy <papateddyetyanga2gmail.com>
2023-09-21 14:40:55 -0700

    lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * _toupper - changes lowercase to uppercase
- * @c: characters the alphabets
+ * string_toupper - changes lowercase to uppercase
+ * @str: characters the alphabets
  * Return: 0
- * 
  */
 char *string_toupper(char *str)
 {
 	int index = 0;
-
-	while (str[index])
 	{
-		if (str[index] >= 'a' && str[index] <= '2')
-			str[index] -= 32;
-		index++;
+	while (str[index])
+	if (str[index] >= 'a' && str[index] <= '2')
+	str[index] -= 32;
+	index++;
 	}
 	return (str);
 }

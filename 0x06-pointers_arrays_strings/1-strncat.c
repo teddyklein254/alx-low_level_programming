e940d887e8b2ebdd68feb68f08b45bb5dc9b04e1
Teddy <papateddyetyanga2gmail.com>
2023-09-21 12:43:50 -0700

    Function concatanating two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - function similar to strcat with n bytes
+ * @src: it uses bytes to _strncat
+ * @n: null terminated
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, k;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	for (k = 0; k < n && src[k] != '\0'; k++)
+		dest[dest_len + k] = src[k];
+	dest[dest_len + k] = '\0';
+	return (dest);
+}

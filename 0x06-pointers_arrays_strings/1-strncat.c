32418d2f5a55e50e48825b35ae94e3149d5be403
Teddy <papateddyetyanga2gmail.com>
2023-09-21 13:12:56 -0700

    concatanating two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,11 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, k;
+	int index = 0, dest_len = 0;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-	for (k = 0; k < n && src[k] != '\0'; k++)
-		dest[dest_len + k] = src[k];
-	dest[dest_len + k] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }

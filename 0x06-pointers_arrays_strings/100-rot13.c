58b53037f9f132f3c470a72d5da110f9e2f3486e
Teddy <papateddyetyanga2gmail.com>
2023-09-22 13:13:49 -0700

    encoding and decoding in rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Contain the encoding characters
+ * @s: characters for encoding
+ */
+char *rot13(char *s)
+{
+	int num1, num2;
+
+	char set1[] = "ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char setrot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (num1 = 0; s[num1] != '\0'; num1++)
+	{
+	for (num2 = 0; num2 < 52; num2++)
+	{
+		if (s[num1] == set1[num2])
+	{
+		s[num1] = setrot[num2];
+	break;
+	}
+	}
+	}
+
+	return (s);
+}

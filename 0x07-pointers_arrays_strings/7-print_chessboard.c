aa49b3a9e36d225a4b6f34ef3fa866b793961c41
Teddy <papateddyetyanga2gmail.com>
2023-09-25 14:09:23 -0700

    Printing chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _chessboard - prints chessboard
+ * @a: assigned
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 8;num1++)
+		for ( num2 = 0; num2 < 8;num2++)
+			putchar(a[num1] [num2]);
+	putchar('\n');
+}

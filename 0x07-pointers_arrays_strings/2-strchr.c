19531472beb348fd4d0071244cd0b99fca2fa6bd
Teddy <papateddyetyanga2gmail.com>
2023-09-25 10:22:50 -0700

    Finding character of string and printing it out

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - string locator
+ * @c: pointer to first occurence
+ * @s: string
+ * Return: NULL if character not found
+ */
+char *_strchr(char *s, char c)
+{
+	int y;
+
+	for (y = 0; s[y] != '\0'; y++)
+		if (s[y] == c)
+			return &s[y];
+			/*printf("%c\n", s[y]);*/
+	return (0);
+}

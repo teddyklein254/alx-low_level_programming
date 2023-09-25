7eb593a00b5b22712951c8e1f2f1b862e64a8fe7
Teddy <papateddyetyanga2gmail.com>
2023-09-25 13:02:55 -0700

    Function for giving and searching characters in string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,13 @@ char *_strpbrk(char *s, char *accept)
 	char *ptr;
 
 	if (s == NULL || accept == NULL)
-		return(NULL);
+		return (NULL);
 
 	while (*s != '\0')
 		ptr = accept;
 	while (*ptr != '\0')
 		if (*s == *ptr)
-			return s;
+			return (s);
 	ptr++;
 
 	s++;

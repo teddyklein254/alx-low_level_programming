1ce9efcdb067a09b23ae753b8194d3ce0318eef3
Teddy <papateddyetyanga2gmail.com>
2023-09-25 08:25:33 -0700

    Function for copying

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - Function for copying
+ * @n: Bytes to be copied from
+ * @src: Bytes that are copied
+ * @dest: Bytes copied to
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+		dest[j] = src[j];
+	return (dest);
+}

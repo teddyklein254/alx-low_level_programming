1c0eb67f109528df7f4cf1e05a29712a4a1566a9
Teddy <papateddyetyanga2gmail.com>
2023-09-26 10:37:08 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,6 +11,8 @@ void print_chessboard(char (*a)[8])
 
 	for (num1 = 0; num1 < 8; num1++)
 		for (num2 = 0; num2 < 8; num2++)
-			putchar(a[num1][num2]);
+		putchar(a[num1][num2]);
+	for (num2 = 8; num2 < 72; num2++)
+		putchar(' ');
 	putchar('\n');
 }

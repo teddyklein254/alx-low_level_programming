2f2cbd47ae12f0f5328c21483d6bcb9dd0718d48
Teddy <papateddyetyanga2gmail.com>
2023-09-28 15:33:38 -0700

    Function for returnig palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - length of string
- * @: the string
+ * @s: the string
  * Return: string that wasnt returned
  */
 int _strlen_recursion(char *s)
@@ -38,5 +38,5 @@ int is_palindrome(char *s)
 {
 	if  (*s == '\0')
 		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	return (comparator(sa, 0, _strlen_recursion(s) - 1));
 }

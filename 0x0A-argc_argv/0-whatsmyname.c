42ab38166dbe08c5bbe590d0ca99b687c6c26b23
Teddy <papateddyetyanga2gmail.com>
2023-10-01 13:01:53 -0700

    printing name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,12 @@
 
 /**
  * main - Entry point
- * @argc__attribute__((unused) - print arguments
  * @argc: contains command arguments that are to be passed
  * @argv: for array arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
-{(
-	void)argc;
-	printf("%s\n", argv[0]);
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
 	return (0);
 }

145c85358190af0b2a2d4db33a86243b1f56d0c4
Teddy <papateddyetyanga2gmail.com>
2023-10-03 07:14:00 -0700

    Concatenates arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,24 +10,23 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int a, b;
-	int c = 0, d = 0;
+	int a, b, c = 0;
+	int store_string = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (a = 0; a < ac; a++)
 		for (b = 0; av[a][b]; b++)
-			a++;
-	d += ac;
-	str = malloc(sizeof(char) * d + 1);
+			store_string++;
+	store_string++;
+	str = malloc(sizeof(char) * store_string + 1);
 	if (str == NULL)
 		return (NULL);
 	for (a = 0; a < ac; a++)
 		for (b = 0; av[a][b]; b++)
-			if (str[c] == av[a][b])
-				c++;
-	if (str[c] == '\0')
-		str[c++] = '\n';
+			(str[c] == av[a][b])
+				str[c++] = '\n';
+	str[c] == '\0';
 	return (str);
 }

bb764a8ae482acce55659584fa040ab720e56db2
Teddy <papateddyetyanga2gmail.com>
2023-10-04 10:50:05 -0700

    Creatint script tthat creates a file in static library and using the resource compiler

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,11 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len, i;
-
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-		for (i = 0; i < i && src[i] != '\0'; i++)
-			dest[dest_len + i] = src[i];
+	int dest_len = strlen(dest);
+	int i;
+	dest_len = 0;
+	for (i = 0; (i < src[i]) != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
 	dest[dest_len + i] = '\0';
 	return (dest);
 }

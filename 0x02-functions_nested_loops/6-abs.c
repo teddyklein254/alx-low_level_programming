1b85ce57086b98ae64d8f5cb66a180acdffda802
Teddy <papateddyetyanga2gmail.com>
2023-10-04 04:41:24 -0700

    computing absoluite integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,7 @@
 
 /**
  * _abs - compute absolute value of integer
+ * @c: computed integer
  * Return: 0
  */
 int _abs(int c)
@@ -9,6 +10,7 @@ int _abs(int c)
 	if (c < 0)
 	{
 		int abs_val;
+
 		abs_val = c * -1;
 
 		return (abs_val);

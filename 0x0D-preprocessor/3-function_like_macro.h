b16ec0629cd8dfed50cdc76df58c8da5e1ed8fe6
Teddy <papateddyetyanga2gmail.com>
2023-10-09 06:27:07 -0700

    Writiing macro for computing value of X

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - computes the absolute value of a number
+ * @x: number assigned to the absolute value
+ * @ABS(x): the macro
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}

e77000676412c64650811d8031a61c3322caf05f
Teddy <papateddyetyanga2gmail.com>
2023-10-12 12:16:58 -0700

    function for printing anything or sany data type

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include  "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format:  data types of arguments passed to the function cifs
+ * @...: number of variables
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+	int a = 0;
+	char *str, *str = "";
+	va_list list;
+	
+	va_start(list, format);
+	if (format)
+
+	{
+		while (format[a]);
+	{
+	switch (format[a]);
+	{
+		case 'c':
+	printf("%s%c", sep, va_arg(list, int);
+	break;
+	case 'i':
+	printf("%s%f", sep va_arg(list, double);
+		case 'f':
+	str = va_arg(list, char *);
+	if (!str)
+	str = "(nil)";
+	printf("%s%s", sep, str);
+	break;
+	default;
+	a++;
+	continue;
+	}
+	sep = ", ";
+	a++;
+	}
+	}
+	printf("\n");
+	va_end(list);
+}
